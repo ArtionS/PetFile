c408528e8027a0473bab7a055b47d0d4a173a768
ArtionS <david.sorianoc@gmail.com>
2022-08-20 23:50:55 -0500

    holitas de mar

---
+++ pet/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import PetList, PetDetail, PetCreate, PetUpdate, PetDelete
+
+
+urlpatterns = [
+    path('pets/', PetList.as_view(), name='pet_list'),
+    path('pet/<int:pk>/', PetDetail.as_view(), name='pet_detail'),
+    path('pet/create/', PetCreate.as_view(), name='pet_create'),
+    path('pet/<int:pk>/update/', PetUpdate.as_view(), name='pet_update'),
+    path('pet/<int:pk>/delete/', PetDelete.as_view(), name='pet_delete'),
+]
---
+++ vaccine/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Vaccine
+
+# Register your models here.
+
+
+admin.site.register(Vaccine)
