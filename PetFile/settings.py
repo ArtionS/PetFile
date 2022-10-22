c87d9dc742945bc09a5eca95b7c31205fb6c2c52
Jaime Soriano <jaime.x.soriano.-nd@disney.com>
2022-10-22 03:43:03 -0500

    changed storage to use s3 bucket

--- PetFile/settings.py
+++ PetFile/settings.py
@@ -15,9 +15,28 @@ import os.path
 from pathlib import Path
 
 # File path
-FILE_DIR = Path(__file__).resolve().parent.parent.parent
-MEDIA_URL = '/Files/'
-MEDIA_ROOT = os.path.join(FILE_DIR, "Files")
+USE_S3 = os.getenv('USE_S3') == 'TRUE'
+
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'PetFile.storages.StaticStorage'
+
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'PetFile.storages.PublicMediaStorage'
+else:
+    FILE_DIR = Path(__file__).resolve().parent.parent.parent
+    MEDIA_URL = '/Files/'
+    MEDIA_ROOT = os.path.join(FILE_DIR, "Files")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +63,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 
     # My Apps
     # aplicacion para las masotas
--- run-local.sh
+++ run-local.sh
@@ -6,5 +6,9 @@ export DBUSER=petfile
 export DBPASS=PetFile.SOCA.2022
 export DBHOST=petfile.chkkb7hsmygj.us-east-1.rds.amazonaws.com
 
+export AWS_STORAGE_BUCKET_NAME=petfile-static-data
+export USE_S3=TRUE
+
+
 docker compose up
 #python manage.py runserver 0.0.0.0:80
